6892517ec2fb5e25c6af1a790ac6e03d21111b6d
Josiely <josiely.araujo21@gmail.com>
2024-05-07 16:05:20 -0400

    delete sits filter

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -23,19 +23,10 @@ function CarList({
   const [viewCar, setViewCar] = useState([]);
 
   const [typeFilterSelected, setTypeFilterSelected] = useState("");
-  const [typeSitsSelected, setTypeSitsSelected] = useState([]);
   const [menuIsOpen, setMenuIsOpen] = useState(false);
   const [carsTypes, setCarsTypes] = useState([]);
   const [carsPerPage, setCarsPerPage] = useState(6);
   const [currentPage, setCurrentPage] = useState(1);
-
-  const filtroPorAsientos = [
-    { texto: "Dos Asientos", valor: 2 },
-    { texto: "Cuatro Asientos", valor: 4 },
-    { texto: "Cinco Asientos", valor: 5 },
-    { texto: "Menos de 10 Asientos", valor: 10 },
-    { texto: "Más de 10 Asientos", valor: 11 },
-  ];
 
   const carsRef = useRef(null);
 
@@ -55,27 +46,11 @@ function CarList({
   };
 
   const onFilter = () => {
-    if (!typeFilterSelected && typeSitsSelected.length < 1) {
+    if (!typeFilterSelected) {
       getCars();
-    } else if (typeFilterSelected && typeSitsSelected.length < 1) {
+    } else if (typeFilterSelected) {
       const filteringCars = filterByMarca();
       setFilteredCars(filteringCars);
-    } else if (!typeFilterSelected && typeSitsSelected.length > 0) {
-      const carsfilteredbysits = filterBySits(typeSitsSelected[1]);
-      setFilteredCars(carsfilteredbysits);
-    } else if (typeFilterSelected && typeSitsSelected.length > 0) {
-      const filteredbysits = filterBySits(typeSitsSelected[1]);
-      if (typeFilterSelected == "Sin marca") {
-        const filteredbymarca = filteredbysits.filter(
-          (car) => car.marca == undefined,
-        );
-        setFilteredCars(filteredbymarca);
-      } else {
-        const filteredbymarca = filteredbysits.filter(
-          (car) => car.marca == typeFilterSelected,
-        );
-        setFilteredCars(filteredbymarca);
-      }
     }
   };
   const filterByMarca = () => {
@@ -86,19 +61,6 @@ function CarList({
       filteringCars = cars.filter((car) => car.marca == typeFilterSelected);
     }
     return filteringCars;
-  };
-  const filterBySits = (i) => {
-    let filter;
-    if (filtroPorAsientos[i].valor < 10) {
-      filter = cars.filter((car) => car.asientos == filtroPorAsientos[i].valor);
-    } else if (filtroPorAsientos[i].valor == 10) {
-      filter = cars.filter((car) => car.asientos < filtroPorAsientos[i].valor);
-    } else if (filtroPorAsientos[i].valor == 11) {
-      filter = cars.filter(
-        (car) => car.asientos >= filtroPorAsientos[i].valor - 1,
-      );
-    }
-    return filter;
   };
 
   const getCars = async () => {
@@ -134,7 +96,7 @@ function CarList({
   useEffect(() => {
     onFilter();
     getCarsType();
-  }, [typeFilterSelected, typeSitsSelected]);
+  }, [typeFilterSelected]);
   if (viewCar.length > 0) {
     return <Carousel car={viewCar[0]} setViewCar={setViewCar} />;
   } else {
@@ -157,24 +119,19 @@ function CarList({
           className={`${menuIsOpen ? "" : "invisible md:visible"} absolute right-5 mt-20 w-[90%] bg-white transition-all duration-200 ease-in-out md:static md:-mt-0 md:w-1/3`}
         >
           <div
-            className={`${typeFilterSelected || typeSitsSelected.length > 0 ? "mb-2 flex items-center gap-2 text-black" : "hidden"}`}
+            className={`${typeFilterSelected ? "mb-2 flex items-center gap-2 text-black" : "hidden"}`}
           >
             <h2
               className={`${typeFilterSelected ? "rounded-md  bg-myred p-2 font-semibold uppercase text-white" : "hidden"}`}
             >
               {typeFilterSelected}
             </h2>
-            <h2
-              className={`${typeSitsSelected.length > 0 ? "rounded-md  bg-myred p-2 font-semibold uppercase text-white" : "hidden"}`}
-            >
-              {typeSitsSelected[0]}
-            </h2>
           </div>
           <section className="">
             <header className="flex h-[60px] place-content-center items-center bg-myred text-center text-white">
               <h2 className="text-lg font-semibold">Encuentra tu vehículo</h2>
             </header>
-            <section className="max-h-[500px] overflow-y-scroll border-x-2 p-4">
+            <section className="max-h-[500px] overflow-y-scroll border-x-2 border-b-2 p-4">
               <ul className="space-y-2">
                 <li>
                   <button
@@ -200,43 +157,6 @@ function CarList({
                       className={`${typeFilterSelected == tipo ? "bg-myred text-white" : ""} flex w-full place-content-between items-center p-2 uppercase`}
                     >
                       {tipo}
-                      <FaPlus />
-                    </button>
-                  </li>
-                ))}
-              </ul>
-            </section>
-          </section>
-          <section>
-            <header className="flex h-[50px] place-content-center items-center bg-myred text-center text-white">
-              <h2 className="text-lg font-semibold">Filtros</h2>
-            </header>
-            <section className="mt-4 flex max-h-[500px] flex-col space-y-3 overflow-y-scroll border-x-2 border-b-2 p-4">
-              <ul className="">
-                <li>
-                  <button
-                    onClick={() => {
-                      setTypeSitsSelected([]);
-                      setCurrentPage(1);
-                      toggleMenu();
-                    }}
-                    className={`${typeSitsSelected.length < 1 ? "bg-myred text-white" : "text-myred"} flex w-full place-content-between items-center p-2 uppercase`}
-                  >
-                    Ver todos
-                    <FaPlus />
-                  </button>
-                </li>
-                {filtroPorAsientos.map((asientos, i) => (
-                  <li key={i}>
-                    <button
-                      onClick={() => {
-                        setTypeSitsSelected([asientos.texto, i]);
-                        setCurrentPage(1);
-                        toggleMenu();
-                      }}
-                      className={`${typeSitsSelected[0] == asientos.texto ? "bg-myred text-white" : ""} flex w-full place-content-between items-center p-2 uppercase`}
-                    >
-                      {asientos.texto}
                       <FaPlus />
                     </button>
                   </li>
