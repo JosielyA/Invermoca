53494127ac25fe791969b91d438e60be8dcc450c
JosielyA <josiely.araujo21@gmail.com>
2024-11-14 16:38:28 -0400

    update cars

--- src/data/carsData.jsx
+++ src/data/carsData.jsx
@@ -1,23 +1,9 @@
 import dodgeram1 from "../assets/cars/dodgeram/image1.jpeg";
 import dodgeram2 from "../assets/cars/dodgeram/image2.jpeg";
 import dodgeram3 from "../assets/cars/dodgeram/image3.jpeg";
-import mitsubishieclipse1 from "../assets/cars/mitsubishieclipse/WhatsApp Image 2024-02-07 at 12.32.18 PM (1).jpeg";
-import mitsubishieclipse2 from "../assets/cars/mitsubishieclipse/WhatsApp Image 2024-02-07 at 12.32.18 PM (2).jpeg";
-import mitsubishieclipse3 from "../assets/cars/mitsubishieclipse/WhatsApp Image 2024-02-07 at 12.32.18 PM.jpeg";
-import aveolt1 from "../assets/cars/aveolt/image1.jpeg";
-import aveolt2 from "../assets/cars/aveolt/image2.jpeg";
-import aveolt3 from "../assets/cars/aveolt/image3.jpeg";
-import aveolt4 from "../assets/cars/aveolt/image4.jpeg";
-import aveolt5 from "../assets/cars/aveolt/image5.jpeg";
 import toyotatacoma1 from "../assets/cars/toyotatacoma/image0.jpeg";
 import toyotatacoma2 from "../assets/cars/toyotatacoma/image1.jpeg";
 import toyotatacoma3 from "../assets/cars/toyotatacoma/image2.jpeg";
-import chevroletorlado1 from "../assets/cars/Chevrolet Orlando/20240814_122221.jpg";
-import chevroletorlado2 from "../assets/cars/Chevrolet Orlando/20240814_122256.jpg";
-import chevroletorlado3 from "../assets/cars/Chevrolet Orlando/20240814_122310.jpg";
-import tahoeblindada1 from "../assets/cars/Chevrolet Tahoe Blindada/20240815_104501.jpg";
-import tahoeblindada2 from "../assets/cars/Chevrolet Tahoe Blindada/20240815_104523.jpg";
-import tahoeblindada3 from "../assets/cars/Chevrolet Tahoe Blindada/20240815_104539.jpg";
 import fordexplorer1 from "../assets/cars/Ford Explorer/PHOTO-2024-08-08-18-10-09.jpg";
 import fordexplorer2 from "../assets/cars/Ford Explorer/PHOTO-2024-08-08-18-10-09_1.jpg";
 import fordexplorer3 from "../assets/cars/Ford Explorer/PHOTO-2024-08-08-18-10-09_2.jpg";
@@ -36,18 +22,45 @@ import fordtransit8 from "../assets/cars/Ford Transit/Imagen de WhatsApp 2024-08
 import jeepcherokee1 from "../assets/cars/Jeep Cherokee KK 2010/Imagen de WhatsApp 2024-08-08 a las 18.11.06_397210c1.jpg";
 import jeepcherokee2 from "../assets/cars/Jeep Cherokee KK 2010/Imagen de WhatsApp 2024-08-08 a las 18.11.06_46528156.jpg";
 import jeepcherokee3 from "../assets/cars/Jeep Cherokee KK 2010/Imagen de WhatsApp 2024-08-08 a las 18.11.06_cc5980dd.jpg";
-import nissanmurano1 from "../assets/cars/Nissan Murano/20240814_135014.jpg";
-import nissanmurano2 from "../assets/cars/Nissan Murano/20240814_135308.jpg";
-import nissanmurano3 from "../assets/cars/Nissan Murano/20240814_135324.jpg";
 import renaultkangoo1 from "../assets/cars/Renault Kangoo/Imagen de WhatsApp 2024-08-15 a las 10.02.33_3ef0d10f.jpg";
 import renaultkangoo2 from "../assets/cars/Renault Kangoo/Imagen de WhatsApp 2024-08-15 a las 10.02.33_8217e6e7.jpg";
 import renaultkangoo3 from "../assets/cars/Renault Kangoo/Imagen de WhatsApp 2024-08-15 a las 10.02.33_f7f126b9.jpg";
-import renaultsymbol1 from "../assets/cars/Renault Symbol/Imagen de WhatsApp 2024-08-14 a las 14.02.18_324551f4.jpg";
-import renaultsymbol2 from "../assets/cars/Renault Symbol/Imagen de WhatsApp 2024-08-14 a las 14.02.18_2bf9e6c8.jpg";
-import renaultsymbol4 from "../assets/cars/Renault Symbol/Imagen de WhatsApp 2024-08-14 a las 14.02.58_c630db21.jpg";
-import renaultsymbol5 from "../assets/cars/Renault Symbol/Imagen de WhatsApp 2024-08-14 a las 14.02.59_0f3977b9.jpg";
+import babycamry1 from "../assets/cars/Baby Camry/image0.jpeg";
+import babycamry2 from "../assets/cars/Baby Camry/image1.jpeg";
+import vitara1 from "../assets/cars/ChevroletVitara/image0.jpeg";
+import vitara2 from "../assets/cars/ChevroletVitara/image1.jpeg";
+import fordtransitotro1 from "../assets/cars/fordtransit2/image0.jpeg";
+import fordtransitotro2 from "../assets/cars/fordtransit2/image1.jpeg";
+import fordtransit20161 from "../assets/cars/Ford transit 2016/image0.jpeg";
+import fordtransit20162 from "../assets/cars/Ford transit 2016/image1.jpeg";
+import runner1 from "../assets/cars/toyota4Runner/image0.jpeg";
+import runner2 from "../assets/cars/toyota4Runner/image1.jpeg";
+import rey1 from "../assets/cars/Rey camión/image0.jpeg";
+import rey2 from "../assets/cars/Rey camión/image1.jpeg";
+import rey3 from "../assets/cars/Rey camión/image2.jpeg";
+import runner2022 from "../assets/cars/toyota4Runner2022/IMG_4950.jpeg";
+import runner20221 from "../assets/cars/toyota4Runner2022/IMG_4951.jpeg";
+import runner20222 from "../assets/cars/toyota4Runner2022/IMG_4952.jpeg";
 
 export const carros = [
+  {
+    id: 0,
+    type: "Camioneta",
+    name: "4 Runner 2022",
+    marca: "Toyota",
+    year: 2022,
+    kilometraje: 22000,
+    image: [runner2022, runner20221, runner20222],
+  },
+  {
+    id: 1,
+    type: "Carro",
+    name: "Baby Camry",
+    marca: "Toyota",
+    year: 2000,
+    description: "IMPORTADO // AUTOMÁTICO",
+    image: [babycamry1, babycamry2],
+  },
   {
     id: 2,
     type: "Camioneta",
@@ -62,27 +75,21 @@ export const carros = [
   },
   {
     id: 3,
-    type: "Carro",
-    name: "Eclipse",
-    marca: "Mitsubishi",
-    price: 4000,
-    year: 1999,
-    kilometraje: 167000,
-    description:
-      "DOCUMENTACION 3-2 // CAUCHOS Y BATERIA NUEVA // DOS JUEGOS DE LLAVE // 5 PUESTOS",
-    image: [mitsubishieclipse2, mitsubishieclipse1, mitsubishieclipse3],
+    type: "Camioneta",
+    name: "Vitara",
+    marca: "Chevrolet",
+    year: 2005,
+    kilometraje: 231000,
+    image: [vitara1, vitara2],
   },
   {
     id: 4,
-    type: "Carro",
-    name: "LT",
-    marca: "Aveo",
-    price: 12500,
-    year: 2015,
-    state: "Nuevo",
-    kilometraje: 55000,
-    description: "2-1 // 5 PUESTOS",
-    image: [aveolt2, aveolt4, aveolt1, aveolt5, aveolt3],
+    type: "Camioneta",
+    name: "Transit",
+    marca: "Ford",
+    year: 2017,
+    kilometraje: 48700,
+    image: [fordtransitotro1, fordtransitotro2],
   },
   {
     id: 5,
@@ -94,33 +101,32 @@ export const carros = [
   },
   {
     id: 6,
-    name: "Symbol",
-    marca: "Renault",
-    type: "Carro",
-    price: 2800,
-    year: 2002,
-    kilometraje: 355000,
-    image: [renaultsymbol1, renaultsymbol2, renaultsymbol4, renaultsymbol5],
+    type: "Camioneta",
+    name: "Transit 2016",
+    marca: "Ford",
+    year: 2016,
+    kilometraje: 188000,
+    description: "6 cil",
+    image: [fordtransit20161, fordtransit20162],
   },
   {
     id: 7,
-    name: "Murano",
-    marca: "Nissan",
     type: "Camioneta",
-    price: 6000,
-    description: "4X5",
-    year: 2005,
-    kilometraje: 139000,
-    image: [nissanmurano3, nissanmurano2, nissanmurano1],
+    name: "4 Runner TDR Pro",
+    marca: "Toyota",
+    year: 2017,
+    kilometraje: 87000,
+    image: [runner1, runner2],
   },
   {
     id: 8,
-    name: "Orlando",
+    type: "Camion",
+    name: "Silverado Rey Camión",
     marca: "Chevrolet",
-    type: "Camioneta",
-    year: 2013,
-    kilometraje: 107000,
-    image: [chevroletorlado3, chevroletorlado2, chevroletorlado1],
+    year: 2012,
+    kilometraje: 193000,
+    description: "4x4",
+    image: [rey1, rey2, rey3],
   },
   {
     id: 9,
@@ -183,15 +189,6 @@ export const carros = [
     year: 2011,
     kilometraje: 150000,
     image: [forderanger2, forderanger1],
-  },
-  {
-    id: 15,
-    name: "Tahoe Blindada",
-    marca: "Chevrolet",
-    type: "Camioneta",
-    year: 2013,
-    kilometraje: 18000,
-    image: [tahoeblindada1, tahoeblindada3, tahoeblindada2],
   },
 ];
 
