0700c62fa1f76aa967df0b4d9fc9f48c4c5a6014
JosielyA <josiely.araujo21@gmail.com>
2024-11-14 14:54:05 -0400

    cars change

--- src/data/carsData.jsx
+++ src/data/carsData.jsx
@@ -12,6 +12,40 @@ import aveolt5 from "../assets/cars/aveolt/image5.jpeg";
 import toyotatacoma1 from "../assets/cars/toyotatacoma/image0.jpeg";
 import toyotatacoma2 from "../assets/cars/toyotatacoma/image1.jpeg";
 import toyotatacoma3 from "../assets/cars/toyotatacoma/image2.jpeg";
+import chevroletorlado1 from "../assets/cars/Chevrolet Orlando/20240814_122221.jpg";
+import chevroletorlado2 from "../assets/cars/Chevrolet Orlando/20240814_122256.jpg";
+import chevroletorlado3 from "../assets/cars/Chevrolet Orlando/20240814_122310.jpg";
+import tahoeblindada1 from "../assets/cars/Chevrolet Tahoe Blindada/20240815_104501.jpg";
+import tahoeblindada2 from "../assets/cars/Chevrolet Tahoe Blindada/20240815_104523.jpg";
+import tahoeblindada3 from "../assets/cars/Chevrolet Tahoe Blindada/20240815_104539.jpg";
+import fordexplorer1 from "../assets/cars/Ford Explorer/PHOTO-2024-08-08-18-10-09.jpg";
+import fordexplorer2 from "../assets/cars/Ford Explorer/PHOTO-2024-08-08-18-10-09_1.jpg";
+import fordexplorer3 from "../assets/cars/Ford Explorer/PHOTO-2024-08-08-18-10-09_2.jpg";
+import forderanger1 from "../assets/cars/Ford Ranger/Imagen de WhatsApp 2024-08-13 a las 18.33.41_6d456a8e.jpg";
+import forderanger2 from "../assets/cars/Ford Ranger/Imagen de WhatsApp 2024-08-13 a las 18.33.41_7087f009.jpg";
+import fordsuperduty1 from "../assets/cars/Ford Súper Duty/image0.jpeg";
+import fordsuperduty2 from "../assets/cars/Ford Súper Duty/image1.jpeg";
+import fordsuperduty3 from "../assets/cars/Ford Súper Duty/image2.jpeg";
+import fordtransit2 from "../assets/cars/Ford Transit/Imagen de WhatsApp 2024-08-15 a las 09.58.58_67fef010.jpg";
+import fordtransit3 from "../assets/cars/Ford Transit/Imagen de WhatsApp 2024-08-15 a las 09.59.15_e824d5e6.jpg";
+import fordtransit4 from "../assets/cars/Ford Transit/Imagen de WhatsApp 2024-08-15 a las 09.59.16_183e9cfd.jpg";
+import fordtransit5 from "../assets/cars/Ford Transit/Imagen de WhatsApp 2024-08-15 a las 09.59.16_c7994590.jpg";
+import fordtransit6 from "../assets/cars/Ford Transit/Imagen de WhatsApp 2024-08-15 a las 09.59.16_d64ff278.jpg";
+import fordtransit7 from "../assets/cars/Ford Transit/Imagen de WhatsApp 2024-08-15 a las 09.59.16_e645b3f0.jpg";
+import fordtransit8 from "../assets/cars/Ford Transit/Imagen de WhatsApp 2024-08-15 a las 09.59.16_fb0a540e.jpg";
+import jeepcherokee1 from "../assets/cars/Jeep Cherokee KK 2010/Imagen de WhatsApp 2024-08-08 a las 18.11.06_397210c1.jpg";
+import jeepcherokee2 from "../assets/cars/Jeep Cherokee KK 2010/Imagen de WhatsApp 2024-08-08 a las 18.11.06_46528156.jpg";
+import jeepcherokee3 from "../assets/cars/Jeep Cherokee KK 2010/Imagen de WhatsApp 2024-08-08 a las 18.11.06_cc5980dd.jpg";
+import nissanmurano1 from "../assets/cars/Nissan Murano/20240814_135014.jpg";
+import nissanmurano2 from "../assets/cars/Nissan Murano/20240814_135308.jpg";
+import nissanmurano3 from "../assets/cars/Nissan Murano/20240814_135324.jpg";
+import renaultkangoo1 from "../assets/cars/Renault Kangoo/Imagen de WhatsApp 2024-08-15 a las 10.02.33_3ef0d10f.jpg";
+import renaultkangoo2 from "../assets/cars/Renault Kangoo/Imagen de WhatsApp 2024-08-15 a las 10.02.33_8217e6e7.jpg";
+import renaultkangoo3 from "../assets/cars/Renault Kangoo/Imagen de WhatsApp 2024-08-15 a las 10.02.33_f7f126b9.jpg";
+import renaultsymbol1 from "../assets/cars/Renault Symbol/Imagen de WhatsApp 2024-08-14 a las 14.02.18_324551f4.jpg";
+import renaultsymbol2 from "../assets/cars/Renault Symbol/Imagen de WhatsApp 2024-08-14 a las 14.02.18_2bf9e6c8.jpg";
+import renaultsymbol4 from "../assets/cars/Renault Symbol/Imagen de WhatsApp 2024-08-14 a las 14.02.58_c630db21.jpg";
+import renaultsymbol5 from "../assets/cars/Renault Symbol/Imagen de WhatsApp 2024-08-14 a las 14.02.59_0f3977b9.jpg";
 
 export const carros = [
   {
@@ -57,6 +91,107 @@ export const carros = [
     type: "Camioneta",
     kilometraje: 57000,
     image: [toyotatacoma2, toyotatacoma1, toyotatacoma3],
+  },
+  {
+    id: 6,
+    name: "Symbol",
+    marca: "Renault",
+    type: "Carro",
+    price: 2800,
+    year: 2002,
+    kilometraje: 355000,
+    image: [renaultsymbol1, renaultsymbol2, renaultsymbol4, renaultsymbol5],
+  },
+  {
+    id: 7,
+    name: "Murano",
+    marca: "Nissan",
+    type: "Camioneta",
+    price: 6000,
+    description: "4X5",
+    year: 2005,
+    kilometraje: 139000,
+    image: [nissanmurano3, nissanmurano2, nissanmurano1],
+  },
+  {
+    id: 8,
+    name: "Orlando",
+    marca: "Chevrolet",
+    type: "Camioneta",
+    year: 2013,
+    kilometraje: 107000,
+    image: [chevroletorlado3, chevroletorlado2, chevroletorlado1],
+  },
+  {
+    id: 9,
+    name: "Explorer",
+    marca: "Ford",
+    type: "Camioneta",
+    description: "Blindaje nivel 3",
+    year: 2016,
+    kilometraje: 77000,
+    image: [fordexplorer1, fordexplorer2, fordexplorer3],
+  },
+  {
+    id: 10,
+    name: "Transit 250",
+    marca: "Ford",
+    type: "Camioneta",
+    year: 2017,
+    kilometraje: 48700,
+    image: [
+      fordtransit2,
+      fordtransit3,
+      fordtransit4,
+      fordtransit5,
+      fordtransit6,
+      fordtransit7,
+      fordtransit8,
+    ],
+  },
+  {
+    id: 11,
+    name: "Kangoo",
+    marca: "Renault",
+    type: "Camioneta",
+    year: 2012,
+    kilometraje: 147000,
+    image: [renaultkangoo1, renaultkangoo2, renaultkangoo3],
+  },
+  {
+    id: 12,
+    name: "Cherokee kk 4x4",
+    marca: "Jeep",
+    type: "Camioneta",
+    year: 2010,
+    image: [jeepcherokee2, jeepcherokee1, jeepcherokee3],
+  },
+  {
+    id: 13,
+    name: "Súper Duty 4x4",
+    marca: "Ford",
+    type: "Camioneta",
+    year: 2013,
+    kilometraje: 13500,
+    image: [fordsuperduty2, fordsuperduty1, fordsuperduty3],
+  },
+  {
+    id: 14,
+    name: "Ranger",
+    marca: "Ford",
+    type: "Camioneta",
+    year: 2011,
+    kilometraje: 150000,
+    image: [forderanger2, forderanger1],
+  },
+  {
+    id: 15,
+    name: "Tahoe Blindada",
+    marca: "Chevrolet",
+    type: "Camioneta",
+    year: 2013,
+    kilometraje: 18000,
+    image: [tahoeblindada1, tahoeblindada3, tahoeblindada2],
   },
 ];
 
